6d0fd361571d99afcb9298da4fb610b0b43f1bd7
Xiangyu Liu <neulxy@gmail.com>
2022-12-22 14:31:07 +1300

    setting and patching values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,6 +30,19 @@ export class AppComponent implements OnInit {
     });
     // this.signupForm.valueChanges.subscribe((value) => console.log(value));
     this.signupForm.statusChanges.subscribe((status) => console.log(status));
+    this.signupForm.setValue({
+      userData: {
+        username: 'Max',
+        email: 'max@test.com',
+      },
+      gender: 'male',
+      hobbies: [],
+    });
+    this.signupForm.patchValue({
+      userData: {
+        username: 'Anna',
+      },
+    });
   }
 
   onSubmit() {
